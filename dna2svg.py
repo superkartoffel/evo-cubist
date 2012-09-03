acb8bea52a09db0ae34f6b9ba64a6f6f91e69fa9
Oliver Lau <oliver@von-und-fuer-lau.de>
2012-09-03 18:10:17 +0200

    Konverter (DNA>SVG) verbessert.

--- dna2svg.py
+++ dna2svg.py
@@ -8,13 +8,13 @@ f = open(filename, "r")
 dna = json.loads(f.read())
 f.close()
 print "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"
-print "<g transform=\"scale(512)\">"
+print " <g transform=\"scale(%d,%d)\">" % (dna["size"]["width"], dna["size"]["height"])
 for genome in dna["dna"]:
     c = genome["color"]
     vertices = genome["vertices"]
-    print "<path fill=\"rgb(%d,%d,%d)\" fill-opacity=\"%f\" d=\"M %f %f" % (c["r"], c["g"], c["b"], c["a"], vertices[0]["x"], vertices[0]["y"]),
+    print "  <path fill=\"rgb(%d,%d,%d)\" fill-opacity=\"%f\" d=\"M %f %f" % (c["r"], c["g"], c["b"], c["a"], vertices[0]["x"], vertices[0]["y"]),
     for v in vertices[1:]:
         print "L %f %f" % (v["x"], v["y"]),
     print "Z\"/>"
-print "</g>"
+print " </g>"
 print "</svg>\n"
