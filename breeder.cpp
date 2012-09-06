bc503a7106837734250ae431338fcaa2491f90b9
Oliver Lau <oliver@von-und-fuer-lau.de>
2012-09-06 10:17:24 +0200

    mDirty = true, nun bei jedem Breeder::proceed().

--- breeder.cpp
+++ breeder.cpp
@@ -121,12 +121,13 @@ inline bool Breeder::willMutate(unsigned int rate) {
 inline void Breeder::mutate(void)
 {
     mMutation = mDNA;
-    if (willMutate(gBreederSettings.genomeEmergenceProbability()) && mMutation.size() < gBreederSettings.maxGenomes()) {
+    // maybe spawn a new genome
+    if (willMutate(gBreederSettings.genomeEmergenceProbability()) && mMutation.size() < gBreederSettings.maxGenomes())
         mMutation.append(Genome());
-    }
-    if (willMutate(gBreederSettings.genomeKillProbability()) && mMutation.size() > gBreederSettings.minGenomes()) {
+    // maybe kill a genome
+    if (willMutate(gBreederSettings.genomeKillProbability()) && mMutation.size() > gBreederSettings.minGenomes())
         mMutation.remove(MT::random() % mMutation.size());
-    }
+    // mutate all contained genomes
     for (DNAType::iterator genome = mMutation.begin(); genome != mMutation.end(); ++genome)
         genome->mutate();
 }
@@ -145,6 +146,7 @@ void Breeder::proceed(void)
         mDNA = mMutation;
         mDNAMutex.unlock();
         ++mSelected;
+        mDirty = true;
         emit evolved(mGenerated, mDNA, mFitness, mSelected, mGeneration+1);
     }
     ++mGeneration;
@@ -162,7 +164,6 @@ void Breeder::stop(void)
 void Breeder::breed(void)
 {
     mStopped = false;
-    mDirty = true;
     start();
 }
 
