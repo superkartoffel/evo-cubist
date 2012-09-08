57a2afa1722f66a702f54bac22c0e8efed0f7c49
Oliver Lau <oliver@von-und-fuer-lau.de>
2012-09-08 11:17:26 +0200

    SVGReader verbessert/korrigiert.

--- svgreader.cpp
+++ svgreader.cpp
@@ -10,21 +10,39 @@
 void SVGReader::readPath(void)
 {
     Q_ASSERT(mXml.isStartElement() && mXml.name() == "path");
+    bool ok;
     QString fill = mXml.attributes().value("fill").toString();
     // <path fill="rgb(14,9,206)" fill-opacity="0.230442" d="M 0.845225 0.845225 L 0.431106 0.585496 L 0.0788198 0.4925 L 0.0861273 0.692974 Z" />
     QRegExp fill_re("(\\d+),\\s*(\\d+),\\s*(\\d+)");
     fill_re.indexIn(fill);
     const QStringList& rgba = fill_re.capturedTexts();
     QColor color(rgba.at(1).toInt(), rgba.at(2).toInt(), rgba.at(3).toInt());
-    color.setAlphaF(mXml.attributes().value("fill-opacity").toString().toDouble());
+    qreal alpha = mXml.attributes().value("fill-opacity").toString().toDouble(&ok);
+    if (!ok) { // try finding "fill-opacity" in "style" attribute
+        // <path fill="rgb(103,81,97)" style="fill-opacity:0.215686" ... />
+        const QString& style = mXml.attributes().value("style").toString();
+        QRegExp fo_re("fill-opacity:\\s*([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)");
+        fo_re.indexIn(style);
+        alpha = fo_re.capturedTexts().at(1).toDouble(&ok);
+        if (!ok)
+            mXml.raiseError(QObject::tr("fill-opacity not found"));
+    }
+    color.setAlphaF(alpha);
 
     QPolygonF polygon;
-    QRegExp coords_re("([\\d\\.]+)\\s+([\\d\\.]+)");
+    // ... 0.0788198 0.4925 ... 3.687e-4 -0.91112 ...
+    QRegExp coords_re("([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)\\s+([-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?)");
     QString d = mXml.attributes().value("d").toString();
     int pos = 0;
     while ((pos = coords_re.indexIn(d)) >= 0) {
         const QStringList& xy = coords_re.capturedTexts();
-        QPointF point(xy.at(1).toDouble(), xy.at(2).toDouble());
+        const qreal x = xy.at(1).toDouble(&ok);
+        if (!ok)
+            mXml.raiseError(QObject::tr("invalid x coordinate in \"%1\"").arg(xy.at(0)));
+        const qreal y = xy.at(3).toDouble(&ok);
+        if (!ok)
+            mXml.raiseError(QObject::tr("invalid y coordinate in \"%1\"").arg(xy.at(0)));
+        QPointF point(x, y);
         polygon << point;
         d = d.right(d.size() - pos - xy.at(0).size() + 1);
     }
