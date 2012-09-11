bd82511f89894c50aa577307fea936730a9ce9a4
Oliver Lau <oliver@von-und-fuer-lau.de>
2012-09-11 07:23:08 +0200

    Vorbereitungen für Skalierung, Translation und Rotation eines Genoms wieder entfernt.

--- genome.cpp
+++ genome.cpp
@@ -39,31 +39,13 @@ void Genome::mutate(void)
         mPolygon.append(QPointF(MT::random1(), MT::random1()));
     if (willMutate(gBreederSettings.pointKillProbability()) && mPolygon.size() > gBreederSettings.minPointsPerGenome())
         mPolygon.remove(MT::random() % mPolygon.size());
-    switch (0 /*MT::random() % 4*/)
-    {
-    case 1: // rotate
-    {
-        break;
-    }
-    case 2: // translate
-    {
-        break;
-    }
-    case 3: // scale
-    {
-        break;
-    }
-    default:
-    {
-        for (QPolygonF::iterator p = mPolygon.begin(); p != mPolygon.end(); ++p) {
-            if (willMutate(gBreederSettings.pointMutationProbability())) {
-                qreal x2 = p->x() + gBreederSettings.dXY() * (MT::random1() - 0.5);
-                qreal y2 = p->y() + gBreederSettings.dXY() * (MT::random1() - 0.5);
-                p->setX((x2 < 0.0)? 0.0 : ((x2 > 1.0)? 1.0 : x2));
-                p->setY((y2 < 0.0)? 0.0 : ((y2 > 1.0)? 1.0 : y2));
-            }
+    for (QPolygonF::iterator p = mPolygon.begin(); p != mPolygon.end(); ++p) {
+        if (willMutate(gBreederSettings.pointMutationProbability())) {
+            qreal x2 = p->x() + gBreederSettings.dXY() * (MT::random1() - 0.5);
+            qreal y2 = p->y() + gBreederSettings.dXY() * (MT::random1() - 0.5);
+            p->setX((x2 < 0.0)? 0.0 : ((x2 > 1.0)? 1.0 : x2));
+            p->setY((y2 < 0.0)? 0.0 : ((y2 > 1.0)? 1.0 : y2));
         }
-    }
     }
     if (willMutate(gBreederSettings.colorMutationProbability())) {
         const int r = (mColor.red()   + (MT::random() % gBreederSettings.dR()) - gBreederSettings.dR()/2) % 255;
