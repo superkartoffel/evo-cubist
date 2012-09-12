fb24eceb9e79f3497bd3cbdef5a88046ea2ff1ce
Oliver Lau <oliver@von-und-fuer-lau.de>
2012-09-12 20:30:50 +0200

    Circle leicht erweitert.

--- circle.h
+++ circle.h
@@ -8,12 +8,18 @@
 
 class Circle {
 public:
-    Circle(void) : mRadius(0.0) {}
-    Circle(const Circle& other) : mCenter(other.mCenter), mRadius(other.mRadius) {}
+    Circle(void)
+        : mRadius(-1)
+    { /* ... */ }
+    Circle(const Circle& other)
+        : mCenter(other.mCenter)
+        , mRadius(other.mRadius)
+    { /* ... */ }
     Circle(const QPointF& p1, const QPointF& p2, const QPointF& p3);
 
     const QPointF& center(void) const { return mCenter; }
     qreal radius(void) const { return mRadius; }
+    bool isValid(void) const { return mRadius >= 0; }
 
 private: // methods
     qreal from3Points(const QPointF& p1, const QPointF& p2, const QPointF& p3);
